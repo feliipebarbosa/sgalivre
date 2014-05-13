eeadaf4136b42ce31bd24d04f836cfb04d3cab3e
kamilaaguiar <kamila.aguiar.nunes@gmail.com>
2014-05-12 21:38:25 -0300

    Acrescentando calendario

--- modules/sga/agendamento/lib/php/template/TAgendamento.php
+++ modules/sga/agendamento/lib/php/template/TAgendamento.php
@@ -73,6 +73,84 @@ class TAgendamento extends Template{
 		
 
 		<div id="conteudo_servicos">
+		</br>	
+
+		<SCRIPT LANGUAGE="JavaScript">
+			<!-- Begin
+			var day_of_week = new Array('Dom','Seg','Ter','Qua','Qui','Sex','Sab');
+			var month_of_year = new Array('Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro');
+ 
+			var Calendar = new Date();
+ 
+			var year = Calendar.getYear();       // Retorna o ano
+			var month = Calendar.getMonth();    // Retorna mes (0-11)
+			var today = Calendar.getDate();     // Retorna dias (1-31)
+			var weekday = Calendar.getDay();   // Retorna dias (1-31)
+ 
+			var DAYS_OF_WEEK = 7;    // "constant" para o numero de dias na semana
+			var DAYS_OF_MONTH = 31;    // "constant" para o numero de dias no mes
+			var cal;    // Usado para imprimir na tela
+ 
+			Calendar.setDate(1);    // Comecar o calendario no dia '1'
+			Calendar.setMonth(month);    // Comecar o calendario com o mes atual
+ 
+ 
+			var TR_start = '<TR>';
+			var TR_end = '</TR>';
+			var highlight_start = '<TD WIDTH="30"><TABLE CELLSPACING=0 BORDER=1 BGCOLOR=DEDEFF BORDERCOLOR=CCCCCC><TR><TD WIDTH=20><B><CENTER>';
+			var highlight_end   = '</CENTER></TD></TR></TABLE></B>';
+			var TD_start = '<TD WIDTH="30"><CENTER>';
+			var TD_end = '</CENTER></TD>';
+ 
+			cal =  '<TABLE BORDER=1 CELLSPACING=0 CELLPADDING=0 BORDERCOLOR=BBBBBB><TR><TD>';
+			cal += '<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=2>' + TR_start;
+			cal += '<TD COLSPAN="' + DAYS_OF_WEEK + '" BGCOLOR="#EFEFEF"><CENTER><B>';
+			cal += month_of_year[month]  + '   ' + year + '</B>' + TD_end + TR_end;
+			cal += TR_start;
+ 
+			for(index=0; index < DAYS_OF_WEEK; index++)
+			{
+ 
+				if(weekday == index)
+					cal += TD_start + '<B>' + day_of_week[index] + '</B>' + TD_end;
+				else
+					cal += TD_start + day_of_week[index] + TD_end;
+			}
+ 
+			cal += TD_end + TR_end;
+			cal += TR_start;
+ 
+			for(index=0; index < Calendar.getDay(); index++)
+				cal += TD_start + '  ' + TD_end;
+ 
+			for(index=0; index < DAYS_OF_MONTH; index++)
+			{
+				if( Calendar.getDate() > index ){
+  					week_day =Calendar.getDay();
+  					if(week_day == 0)
+  						cal += TR_start;
+  					if(week_day != DAYS_OF_WEEK){
+  						var day  = Calendar.getDate();
+  						if( today==Calendar.getDate() )
+  							cal += highlight_start + day + highlight_end + TD_end;
+  						else
+  							cal += TD_start + day + TD_end;
+  					}
+  					if(week_day == DAYS_OF_WEEK)
+  						cal += TR_end;
+  				}
+  				Calendar.setDate(Calendar.getDate()+1);
+			}
+			cal += '</TD></TR></TABLE></TABLE>';
+ 
+			//  MOSTRAR CALENDARIO
+			document.write(cal);
+			//  End -->
+		</SCRIPT>
+
+		</br>
+		</br>
+
 		<form id="frm_criar_agendamento" method="post" action="" onsubmit="Agendamento.criar_agendamento(); return false;">						
 			<table class="agendamento">
 				<thead>
